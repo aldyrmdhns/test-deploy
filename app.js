a42252ad5e7398d385bf79fd2488868f18a97b6a
Aldy Ramadhan Syahputra <92864319+aldyrmdhns@users.noreply.github.com>
2024-12-08 17:09:48 +0800

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 app.get("/test", (req, res) => {
-	res.send("Its a Success");
+	res.send("Now I try the action and it should be fine!");
 });
 
 app.get("/flight", async (req, res) => {
