db40b8f4dc7c7a57a1aca441c35814ff43e97774
aldyrmdhns <aldyramadhans101@gmail.com>
2024-12-08 16:44:08 +0800

    add code

--- app.js
+++ app.js
@@ -1,16 +1,46 @@
-if (NODE_ENV !== 'production') {
-    require("dotenv").config();
+if (process.envNODE_ENV !== "production") {
+	require("dotenv").config();
 }
 
-const express = require('express')
-const app = express()
-const PORT = process.env.PORT
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT;
+const moment = require("moment-timezone");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
-app.get('/test', (req, res) => {
-    res.send('Its a Success')
-})
+app.get("/test", (req, res) => {
+	res.send("Its a Success");
+});
+
+app.get("/flight", async (req, res) => {
+	try {
+		const flights = await prisma.flights.findMany();
+
+		const timeZone = "Asia/Jakarta";
+		const updatedFlights = flights.map((flight) => {
+			const departureTimeConvert = moment
+				.utc(flight.departureTime)
+				.tz(timeZone)
+				.format("YYYY-MM-DD HH:mm:ss");
+			const arrivalTimeConvert = moment
+				.utc(flight.arrivalTime)
+				.tz(timeZone)
+				.format("YYYY-MM-DD HH:mm:ss");
+
+			return {
+				...flight,
+				departureTime: departureTimeConvert,
+				arrivalTime: arrivalTimeConvert,
+			};
+		});
+
+		res.status(200).json({ updatedFlights });
+	} catch (error) {
+		res.status(400).json({ message: error.message });
+	}
+});
 
 app.listen(PORT, () => {
-    console.log(`-> Listening on port: ${PORT}`);
-})
-
+	console.log(`-> Listening on port: ${PORT}`);
+});
